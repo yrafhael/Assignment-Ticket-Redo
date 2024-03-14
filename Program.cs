2133f59fdd1a2562bfee61bccc1b546c989a8051
Yose Rafhael <yrafhael@my.wctc.edu>
2024-03-14 11:38:03 -0500

    [1.05] Create ReadDataFromFile() -checking if the file is exist -open the file for reading -read line from file -print all name for watching list by joining them (,) -print error if the file does not exist

--- Program.cs
+++ Program.cs
@@ -27,5 +27,67 @@
 
             } while (userChoice == "1" || userChoice == "2");
         }
+
+        static void ReadDataFromFile()
+        {
+            if (File.Exists(file))
+            {
+                bool isFirstLine = true;
+                string[] headerFields = null;
+
+                using (StreamReader sr = new StreamReader(file))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+
+                        if (isFirstLine)
+                        {
+                            headerFields = line.Split(',');
+                            isFirstLine = false;
+                            continue;
+                        }
+
+                        string[] fields = line.Split(',');
+
+                        if (fields.Length >= headerFields.Length) // Allowing for extra data fields
+                        {
+                            for (int i = 0; i < headerFields.Length; i++)
+                            {
+                                if (i < fields.Length)
+                                {
+                                    if (headerFields[i] == "Watching")
+                                    {
+                                        // Print all names in the Watching list
+                                        List<string> watching = fields[i].Split(',').ToList();
+                                        Console.WriteLine($"{headerFields[i]}: {string.Join(", ", watching)}");
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine($"{headerFields[i]}: {fields[i]}");
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"{headerFields[i]}: N/A"); // Handle extra data fields
+                                }
+                            }
+                            Console.WriteLine();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error: Number of fields in a line does not match the number of header fields.");
+                            Console.WriteLine("Header Fields: " + string.Join(", ", headerFields));
+                            Console.WriteLine("Data Fields: " + string.Join(", ", fields));
+                            // Additional handling or logging can be added here
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("File does not exist");
+            }
+        }
     }
 }
