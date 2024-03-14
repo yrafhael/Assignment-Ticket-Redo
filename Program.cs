60228e3445def1abab4dba4b8b12372995e5c51d
Yose Rafhael <yrafhael@my.wctc.edu>
2024-03-14 11:25:59 -0500

    [1.03] Create class Program -create Main method -create Tickets.csv file

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Ticketing
+{
+    class Program
+    {
+        static string file = "Tickets.csv";
+
+        static void Main(string[] args)
+        {
+            string userChoice;
+
+            do
+            {
+                Console.WriteLine("1) Read data from file.");
+                Console.WriteLine("2) Create file from data.");
+                Console.WriteLine("Enter any other key to exit.");
+
+                userChoice = Console.ReadLine();
+
+                if (userChoice == "1")
+                {
+                    ReadDataFromFile();
+                }
+                else if (userChoice == "2")
+                {
+                    CreateFileFromData();
+                }
+
+            } while (userChoice == "1" || userChoice == "2");
+        }
+    }
+}
